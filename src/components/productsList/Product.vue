283bc1a1dc9327560587b0dcca59b5083940d5a7
Habra26 <arnaud.habraken@outlook.com>
2025-12-05 21:26:44 +0100

    Création du component Product et utilisation dans ProductList

---
+++ src/components/productsList/Product.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  product: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <article class="bg-white rounded-lg overflow-hidden shadow-md">
+    <img
+      :src="product.image"
+      :alt="product.name"
+      class="w-full h-40 object-cover"
+    />
+
+    <div class="p-4">
+      <h2 class="font-bold text-lg mb-2">
+        {{ product.name }}
+      </h2>
+
+      <p class="text-gray-700">€{{ Number(product.price).toFixed(2) }}</p>
+
+      <button
+        class="mt-4 w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Ajouter
+      </button>
+    </div>
+  </article>
+</template>
--- src/components/productsList/ProductsList.vue
+++ src/components/productsList/ProductsList.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { onMounted } from 'vue';
-import DB from '@/services/DB';
-import { productsStore } from '@/stores/products';
+import { onMounted } from "vue";
+import DB from "@/services/DB";
+import { productsStore } from "@/stores/products";
+import Product from "./Product.vue";
 
 const props = defineProps({
   apiURL: { type: String, required: true },
@@ -17,26 +18,11 @@ onMounted(async () => {
   <!-- Products Section -->
   <h1 class="text-3xl font-bold mb-4">Nouveaux produits</h1>
   <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-    <!-- Product card example -->
-    <article 
-    class="bg-white rounded-lg overflow-hidden shadow-md"
-    v-for="product in productsStore.products"
-    :key="product.id">
-      <img
-        :src="product.image"
-        :alt="product.name"
-        class="w-full h-48"
-      />
-      <div class="p-4">
-        <h2 class="font-bold text-lg mb-2">{{ product.name }}</h2>
-        <p class="text-gray-700">€{{ Number(product.price).toFixed(2) }}</p>
-        <button
-          class="mt-4 w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Ajouter
-        </button>
-      </div>
-    </article>
+    <Product
+      v-for="product in productsStore.products"
+      :key="product.id"
+      :product="product"
+    />
   </div>
 </template>
 
